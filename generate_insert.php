5cb898fa2047c3d28abd0f2697385cc774c2f573
Richard Pollock <rich.pollock@gmail.com>
2011-10-25 09:22:43 +0200

    Code tidy-up
    Folded the code to handle nodes with no children into an if block along
    with the code blocks that execute when the indentation increases or
    decreases

--- generate_insert.php
+++ generate_insert.php
@@ -1,27 +1,20 @@
 <?php
 $lines = file("input_file_path.txt");
 $sql_insert = "";
-$indentation_changed = false;
 $relationship = 0;
-$index_of_last_parent = array();
+$index_of_last_parent_at_level = array();
 $tree_nodes = array();
 $prev_indentation_level = -1;
 
-// $tree_nodes 0 = parent, 1 = node, 2 = left, 3 = right, 4 = indentation
-
 for ($i = 0; $i <= count($lines); $i++) {
     $indentation_level = strspn($lines[$i],"\t");
-	$indentation_changed = false;
 	
 	if ($indentation_level > $prev_indentation_level) {
 		// Indentation increased
-		$indentation_changed = true;
 		$relationship++;
-		$index_of_last_parent[$indentation_level] = $i-1;
-	}
-	if ($indentation_level < $prev_indentation_level) {
+		$index_of_last_parent_at_level[$indentation_level] = $i-1;
+	} else if ($indentation_level < $prev_indentation_level) {
 		// Indentation decreased
-		$indentation_changed = true;
 		$tree_nodes[$i-1][2] = $relationship;
 		$relationship++;
 		$tree_nodes[$i-1][3] = $relationship;
@@ -29,21 +22,19 @@ for ($i = 0; $i <= count($lines); $i++) {
 		
 		$indentation_diff = $prev_indentation_level - $indentation_level;
 		for ($j = $indentation_diff; $j > 0; $j--) {
-			$tree_nodes[$index_of_last_parent[$indentation_level+$j]][3] = $relationship;
+			$tree_nodes[$index_of_last_parent_at_level[$indentation_level+$j]][3] = $relationship;
 			$relationship++;
 		}
-	}
-	
-	if ($i != count($lines))
-		$tree_nodes[] = array($indentation_level == 0 ? "" : $lines[$index_of_last_parent[$indentation_level]], $lines[$i], $relationship, 0, $indentation_level);
-	
-	if (!$indentation_changed) {
+	} else {
 		// No change in indentation since the last node
 		$tree_nodes[$i-1][2] = $relationship;
 		$relationship++;
 		$tree_nodes[$i-1][3] = $relationship;
 		$relationship++;
 	}
+	
+	if ($i != count($lines))
+		$tree_nodes[] = array($indentation_level == 0 ? "" : $lines[$index_of_last_parent_at_level[$indentation_level]], $lines[$i], $relationship, 0, $indentation_level);
 
 	$prev_indentation_level = $indentation_level;
 }
